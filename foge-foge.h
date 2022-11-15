a411132fcbf2f1748b4d5f776fcce61dca62ebce
rofnavarro <rofnavarro@gmail.com>
2022-11-14 22:15:29 -0300

    foge-foge_v6

---
+++ foge-foge.h
@@ -0,0 +1,15 @@
+#ifndef FOGE_FOGE_H
+# define FOGE_FOGE_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include "./ft_printf/ft_printf.h"
+
+typedef struct s_mapa
+{
+	char	**mapa;
+	int		y;
+	int		x;
+}	t_mapa;
+
+#endif
